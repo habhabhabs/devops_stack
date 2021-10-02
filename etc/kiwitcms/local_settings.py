ad7cc3795d94415e3f26c57ffdcfa9601f015bb4
kytay <kytay@noreply.com>
2021-10-02 08:25:17 +0000

    Update devops stack

---
+++ etc/kiwitcms/local_settings.py
@@ -0,0 +1,9 @@
+SERVER_EMAIL = DEFAULT_FROM_EMAIL = "kiwi.sgdevlab@noreply.sgdevlab.com"
+EMAIL_SUBJECT_PREFIX = "[Kiwi-SGDevLab] "
+
+#  SMTP specific settings
+EMAIL_HOST = 'smtp-sgdevlab'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
