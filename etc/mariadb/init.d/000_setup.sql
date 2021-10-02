b46e4ade3e542c7f1d7fa86bec332f2828445979
kytay <kytay@noreply.com>
2021-10-02 09:26:48 +0000

    Update devops stack

--- etc/mariadb/init.d/000_setup.sql
+++ etc/mariadb/init.d/000_setup.sql
@@ -5,3 +5,7 @@ GRANT ALL PRIVILEGES ON kiwi.* TO 'kiwidba'@'%';
 CREATE DATABASE gitea CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'giteadba'@'%' IDENTIFIED BY 'DK8BVGaLfEvQOe';
 GRANT ALL PRIVILEGES ON gitea.* TO 'giteadba'@'%';
+
+CREATE DATABASE keycloak CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER 'keycloakdba'@'%' IDENTIFIED BY 'rxBhGqZq1LQg7U';
+GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloakdba'@'%';
